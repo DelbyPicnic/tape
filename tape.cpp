7ab388c2dadd8bb12dd20739975102cb39893fc3
DelbyPicnic <gordonswan@me.com>
2020-06-29 20:28:49 +0100

    added old code into file temporarily

--- tape.cpp
+++ tape.cpp
@@ -16,6 +16,51 @@ template<typename T>
 Tape<T>::loadFile(const std::string fname)
 {
     // load data from wav file into buffer
+    if (!loaded){
+        tapeFile = fopen(tpath.c_str(), "r+");
+        if (tapeFile == nullptr){
+            std::cout << "could not load tape." << std::endl;
+            return false;
+        }
+    
+        int headerSize = sizeof(tape_hdr);
+        int fileLength = 0;
+        size_t samplesRead = fread(&tapeHeader, 1, headerSize, tapeFile);
+
+        if (samplesRead > 0){
+            if (tapeHeader.Subchunk2Size > MAX_TAPE_SIZE){
+                std::cerr << tapeHeader.Subchunk2Size << " - selected file is larger than 10mb" << std::endl;
+                exit(1);
+            }else{
+
+                uint16_t bytesPerSample = tapeHeader.bitsPerSample / 8;      
+                uint64_t numSamples = tapeHeader.ChunkSize / bytesPerSample;
+                static const uint16_t BUFFER_SIZE = 4096;
+                SAMPLE* buffer = new SAMPLE[BUFFER_SIZE];
+
+                
+
+                while ((samplesRead = fread(buffer, sizeof buffer[0], BUFFER_SIZE, tapeFile)) > 0)
+                {
+                    memcpy(data->buffer + _end, buffer, sizeof(buffer[0]) * samplesRead);
+                    _end += samplesRead * sizeof(buffer[0]);
+                    
+                }
+                std::cout << "here" << std::endl;
+                std::cout << _end << " bytes read." << std::endl;
+
+                delete [] buffer;
+                buffer = nullptr;
+                fclose(tapeFile);
+
+                // set tape head position
+                _head = 0; //for now
+
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 template<typename T>
